d7bb15eb6cd7a6ea8b6f0b869eba57f16b896970
Achintha Edirisinghe <it23779570@my.sliit.lk>
2026-06-08 03:37:19 +0530

    style: Reduce logo size further in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-24">
                     <div className="flex-shrink-0 flex items-center">
-                        <Link to="/" className="flex items-center -ml-4">
-                            <img src={logoImg} alt="Raani Cream Logo" className="h-24 w-auto mix-blend-multiply object-contain scale-110" />
+                        <Link to="/" className="flex items-center">
+                            <img src={logoImg} alt="Raani Cream Logo" className="h-20 w-auto mix-blend-multiply object-contain" />
                         </Link>
                     </div>
                     <div className="hidden md:flex space-x-4 items-center">
